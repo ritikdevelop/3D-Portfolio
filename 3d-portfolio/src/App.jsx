d4d3dd5a93f8dfff7d615d7274e7587e75df9b0d
ritikdevelop <hrithikrana216@gmail.com>
2024-09-08 00:58:04 +0530

    First commit in 3D-Portfolio website

---
+++ 3d-portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
---
+++ 3d-portfolio/src/sections/Navbar.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { navLinks } from "../constants";
+
+// const navItems = ['Home', 'About', 'Work', 'Contact']
+const NavItems = () => {
+    return(
+        <ul className="nav-ul">
+            {navLinks.map(({id, href, name})=>(
+                <li key={id}></li>
+            ))}
+            {/* <li className="nav-li">
+                <a href="/" className="nav-li_a">Home</a>
+            </li>
+            <li className="nav-li">
+                <a href="/" className="nav-li_a">About</a>
+            </li>
+            <li className="nav-li">
+                <a href="/" className="nav-li_a">Skills</a>
+            </li>
+            <li className="nav-li">
+                <a href="/" className="nav-li_a">Projects</a>
+            </li>
+            <li className="nav-li">
+                <a href="/" className="nav-li_a">Contact</a>
+            </li> */}
+        </ul>
+    )
+}
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen);
+
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center py-5 mx-auto c-space">
+          <a
+            href="/"
+            className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
+          >
+            Ritik
+          </a>
+
+          <button
+            onClick={toggleMenu}
+            className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+            aria-label="Toggle menu"
+          >
+            <img
+              src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
+              alt="toggle"
+              className="w-6 h-6"
+            />
+          </button>
+          <nav className="sm:flex hidden">
+            <NavItems />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
